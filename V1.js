968637ccf942874d52bad0bc70f768cee9dcd862
0dpe <149327603+0dpe@users.noreply.github.com>
2024-04-18 22:29:20 -0400

    Update V1.js

--- V1.js
+++ V1.js
@@ -14,8 +14,8 @@
 (function () {
     'use strict'
 
-    const style1 = 'font-weight: bold';
-    const style2 = 'text-decoration: line-through color: grey'
+    const style1 = 'font-weight: bold'
+    const style2 = 'text-decoration: line-through; color: grey'
 
     const findAndClick = (identifier) => {
         console.log(`%cMA: Attempting ${identifier}`, style2)
@@ -32,7 +32,7 @@
         const currentHash = window.location.hash
 
         if (currentHash === '#studentmyday/assignment-center') {
-            console.log('%cMA: Assignment Center Hash', style1);
+            console.log('%cMA: Assignment Center Hash', style1); // This semicolon is REQUIRED, no idea why ¯\_(ツ)_/¯
             ['[data-sort="date_due"]', '[data-sort="date_due"]', '#month-view', '#filter-status', '.status-button.active[data-id="1"]', '#btn-filter-apply'].forEach(findAndClick)
         }
 
