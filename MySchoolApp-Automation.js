7f654b6981862bea0f4e862e683d6a5da3f3ea58
0dpe <149327603+0dpe@users.noreply.github.com>
2024-04-20 15:26:03 -0400

    Update and rename V1.js to MySchoolApp-Automation.js

--- MySchoolApp-Automation.js
+++ MySchoolApp-Automation.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         MySchoolApp Automation (MA)
+// @name         MySchoolApp Automation
 // @namespace    https://github.com/0dpe/MySchoolApp-UserScript
 // @version      1.0
 // @description  Automatically clicks buttons in the Blackbaud MySchoolApp portal.
