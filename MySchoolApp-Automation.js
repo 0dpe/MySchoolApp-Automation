0f28ccac0847e36e4198aefe2041a684419d3665
0dpe <149327603+0dpe@users.noreply.github.com>
2024-10-11 15:28:33 -0400

    Create MySchoolApp-Automation.js

---
+++ MySchoolApp-Automation.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         MySchoolApp Automation
+// @namespace    https://github.com/0dpe/MySchoolApp-Automation
+// @version      2.0
+// @description  Automatically clicks buttons in the Blackbaud MySchoolApp student portal.
+// @author       Odpe
+// @match        https://*.myschoolapp.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/0dpe/MySchoolApp-Automation/main/MySchoolApp-Automation.js
+// @updateURL    https://raw.githubusercontent.com/0dpe/MySchoolApp-Automation/main/MySchoolApp-Automation.js
+// ==/UserScript==
+
+(function () {
+    'use strict'
+
+    const consoleStyle = 'font-weight: bold'
+    const urls = {
+        assignmentCenter: 'https://worcesteracademy.myschoolapp.com/lms-assignment/assignment-center/student',
+        login: 'https://worcesteracademy.myschoolapp.com/app/student#login'
+    }
+
+    const observeNewElements = (callback) => {
+        new MutationObserver(callback).observe(document.body, { subtree: true, childList: true })
+    }
+
+    if (location.href === urls.assignmentCenter) {
+        console.log(`%cMA: Assignment Center URL`, consoleStyle)
+        observeNewElements(() => {
+            const listView = document.querySelector('sky-radio[icon="list"]:not([MA-data-clicked]) input[type="radio"]')
+            if (listView) {
+                listView.click()
+                listView.closest('sky-radio').setAttribute('MA-data-clicked', 'true')
+                console.log(`%cMA Clicked List View:`, consoleStyle, listView)
+            }
+            const completedCheckbox = Array.from(document.querySelectorAll("span.ng-star-inserted:not([MA-data-clicked])"))
+                .find(el => el.textContent.includes("Completed"))
+                ?.closest("label.sky-checkbox-wrapper.sky-switch")
+            if (completedCheckbox && completedCheckbox.querySelector('sky-icon[icon="check"]')) {
+                completedCheckbox.click()
+                completedCheckbox.querySelector("span.ng-star-inserted").setAttribute('MA-data-clicked', 'true')
+                console.log(`%cMA Clicked Completed Checkbox:`, consoleStyle, completedCheckbox)
+            }
+        })
+    }
+
+    if (location.href === urls.login) {
+        console.log(`%cLogin URL`, consoleStyle)
+        observeNewElements(() => {
+            const nextButton = document.querySelector('input[value="Next"]')
+            const rememberMeCheckbox = document.querySelector('input[checked="checked"]')
+            const usernameInput = document.querySelector('#Username')
+            if (nextButton && rememberMeCheckbox && usernameInput && usernameInput.value.includes('@')) {
+                nextButton.click()
+                console.log(`%cMA Clicked Next Button:`, consoleStyle, nextButton)
+            }
+        })
+    }
+})()
